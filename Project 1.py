bee4aff5d2a59d1d3eb0db24fc0f992d06ae943d
SteveKann <Steve.Kan@ontario.ca>
2021-09-14 12:11:14 +0800

    updates

---
+++ Project 1.py
@@ -0,0 +1,13 @@
+# Libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+# Loading the datasets
+
+data2 = pd.read_csv("C:/Users/Stevie/Documents/GitHub/dataprojects/who_covid19.csv")
+
+# Looking 
+data2.info()
+
